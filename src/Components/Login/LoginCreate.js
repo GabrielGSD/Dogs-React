e690ee358e9c3d997bc2be4f47835684d377de2f
myusername <gabrieldaniel@gec.inatel.br>
2021-08-19 00:57:15 -0300

    Create form login

---
+++ src/Components/Login/LoginCreate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginCreate() {
+  return (
+    <div>
+      <h1>Cadastrar</h1>
+    </div>
+  );
+}
+
+export default LoginCreate;
---
+++ src/Components/Login/LoginForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import useForm from '../../Hooks/useForm';
+import Button from '../Form/Button/Button';
+import Input from '../Form/Input/Input';
+import { UserContext } from '../../Context/UserContext';
+
+function LoginForm() {
+  const username = useForm();
+  const password = useForm();
+
+  const { userLogin, error, loading } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (username.validate() && password.validate()) {
+      userLogin(username.value, password.value);
+    }
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+        {error && <p>{error}</p>}
+      </form>
+      <Link to="/criar"> Cadastro </Link>
+    </section>
+  );
+}
+
+export default LoginForm;
